d9f40ad76421687958526d156b21a84f3c35098e
thanhtuan12mm <thanhtuan12mm@gmail.com>
2021-08-29 16:02:26 +0700

    Create and destroy sessions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
   end
 
   def index
-    @article = Article.paginate(page: params[:page], per_page: 5)
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def show
