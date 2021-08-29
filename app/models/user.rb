1c58af8ebab81d1ca396f3554dbf0e63a91d5194
thanhtuan12mm <thanhtuan12mm@gmail.com>
2021-08-29 12:25:57 +0700

    Add secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User <ApplicationRecord
   before_save { self.email = email.downcase }
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }
+  has_secure_password
 end
