dc110b56f23388cd7f10389f15e4f33ac3a5d8a8
thanhtuan12mm <thanhtuan12mm@gmail.com>
2021-08-29 17:28:53 +0700

    Restrict actions in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
-  
+  before_action :require_same_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -43,5 +44,12 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit your own account!"
+      redirect_to root_path
+    end
   end
 end
