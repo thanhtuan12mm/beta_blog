2fd696e62b83b2842f37db28f5ffbec70645b697
thanhtuan12mm <thanhtuan12mm@gmail.com>
2021-08-30 11:06:36 +0700

    Create category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
-  validates :name, precence: true, length: { minimum: 3, maximum: 25 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 25 }
   validates_uniqueness_of :name
 end
